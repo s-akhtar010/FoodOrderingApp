b6456b18df950e65787c228f7609cc2b60358b4d
shahnawaz <shahnawazakhtar345@gmail.com>
2024-03-26 12:36:11 +0530

    Slider complete

---
+++ src/Component/Header.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import { FaCartPlus } from "react-icons/fa";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdClose } from "react-icons/md"; 
+ 
+
+export const Header = () => {
+    const [sideNav, setSideNav] = useState(false);
+
+    const handleChange = () => {
+        setSideNav(!sideNav);
+    }
+
+    return (
+        <div className="bg-[rgb(245,246,250)] flex justify-between py-[20px] px-[25px] text-[15px] font-semibold border-b h-[70px] relative">
+            <div>
+                <p>Shahnawaz<span className="text-[#fbc531]">Food</span></p>
+            </div>
+            <div>
+                <ul className="sm:flex gap-4 hidden cursor-pointer">
+                    <li className="hover:text-[#fbc531]">
+                        Home
+                    </li>
+                    <li className="hover:text-[#fbc531]">
+                        About Us
+                    </li>
+                    <li className="hover:text-[#fbc531]">
+                        Offers
+                    </li>
+                    <li className="hover:text-[#fbc531]">
+                        Sign In
+                    </li>
+                </ul>
+            </div>
+
+           {/*  Hamburger Logic */}
+            {sideNav &&
+                <div className="bg-[rgb(245,246,250)] w-[200px] absolute top-[65px] right-0 text-center h-[100vh] flex items-center justify-cente px-[60px]">
+                    <MdClose size={30} className="absolute top-2 right-3 cursor-pointer" onClick={handleChange} />
+                    <ul className="sm:flex gap-12  cursor-pointer flex-col absolute">
+                        <li className="hover:text-[#fbc531]">
+                            Home
+                        </li>
+                        <li className="hover:text-[#fbc531]">
+                            About Us
+                        </li>
+                        <li className="hover:text-[#fbc531]">
+                            offers
+                        </li>
+                        <li className="hover:text-[#fbc531]">
+                            Sign In
+                        </li>
+                    </ul>
+                </div>
+            }
+
+           {/*  Hamburger logic end */}
+            <div className="flex gap-5 cursor-pointer">
+                <GiHamburgerMenu size={30} className="sm:hidden" onClick={handleChange} />
+                <p><FaCartPlus size={30} /></p>
+            </div>
+        </div>
+    )
+}
---
+++ src/Component/Hero.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react';
+import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from "react-icons/fa";
+
+export const Hero = () => {
+    const imgData=[
+        {
+            url:"https://i.pinimg.com/originals/c9/4a/ff/c94affe6bcb9fbf7d3508ba98977a1d1.jpg",
+        },
+        {
+            url:"https://static.vecteezy.com/system/resources/previews/030/316/720/large_2x/cup-holds-dawn-s-aroma-window-backdrop-table-shadow-stretches-coffee-s-awakening-vertical-mobile-wallpaper-ai-generated-free-photo.jpg",
+        },
+        {
+            url:"https://thumbs.dreamstime.com/b/indian-thali-26440162.jpg",
+        }
+    ];
+
+    const [slider, setSlider] = useState(0);
+
+    const handleLeft = () => {
+        setSlider(slider === 0 ? imgData.length - 1 : slider - 1);
+    };
+
+    const handleRight = () => {
+        setSlider(slider === imgData.length - 1 ? 0 : slider + 1);
+    };
+
+   /*  useEffect(()=>{
+       const sliderClear= setInterval(()=>{
+         handleRight()
+        },3000)
+
+        return()=>clearInterval(sliderClear)
+
+    },[slider]) */
+
+    return (
+        <div>
+            <div className="w-[90%] mx-auto h-[80vh] my-5 relative" >
+               
+                <img src={imgData[slider].url} alt="" className=' w-full h-[80vh] object-cover  rounded-3xl border-gray-300'/>
+            </div>
+            <p className="absolute top-[50%] text-center w-full text-gray-100 font-semibold text-xl">
+                Welcome To the Delicious Food
+            </p>
+            <div className="flex justify-between w-full px-5 absolute top-[50%] text-black">
+                <FaArrowAltCircleLeft size={30} className="cursor-pointer" onClick={handleLeft}/>
+                <FaArrowAltCircleRight size={30} className="cursor-pointer" onClick={handleRight}/>
+            </div>
+        </div>
+    );
+};
